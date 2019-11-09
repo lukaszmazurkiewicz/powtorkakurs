6a2afa4e315e96263a2ca8c11ac0d5e050201b10
lukaszmazurkiewicz <manzi@o2.pl>
2019-11-09 19:56:14 +0100

    a

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,35 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie do testowania.");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        Calculator calculator = new Calculator();
+
+        int addResult = calculator.add(10, 5);
+        int subtractResult = calculator.subtract(10, 5);
+
+        if (addResult == 15) {
+            System.out.println("Add test OK");
+        } else {
+            System.out.println("Error");
+        }
+
+        if (subtractResult == 5) {
+            System.out.println("Subtract test OK");
+        } else {
+            System.out.println("Error!");
+        }
     }
 }
